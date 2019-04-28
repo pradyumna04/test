f3b98acde1053757bdac78a87f3b6ab3a3a1d9d0
Pradyumna Singh <pradyumnas@vmware.com>
2019-04-28 20:38:25 +0530

    second

--- src/test/java/com/my/prad/CalculatorTest.java
+++ src/test/java/com/my/prad/CalculatorTest.java
@@ -4,13 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PradApplicationTests {
+public class CalculatorTest {
 
-	@Test
-	public void contextLoads() {
-	}
 
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
 }
